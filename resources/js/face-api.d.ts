50654a1d681570b3dcee3cabd13fa63d00456f59
Olakunlevpn <olakunlevpn@live.com>
2026-03-31 21:16:16 +0100

    Fix face-api.js type declarations for nets, detectSingleFace, TinyFaceDetectorOptions

--- resources/js/face-api.d.ts
+++ resources/js/face-api.d.ts
@@ -1,3 +1,12 @@
 declare module 'face-api.js' {
-    export * from 'face-api.js/build/commonjs';
+    export const nets: {
+        tinyFaceDetector: { loadFromUri(uri: string): Promise<void> };
+        ageGenderNet: { loadFromUri(uri: string): Promise<void> };
+    };
+    export class TinyFaceDetectorOptions {
+        constructor(options?: { inputSize?: number; scoreThreshold?: number });
+    }
+    export function detectSingleFace(input: any, options?: any): {
+        withAgeAndGender(): Promise<any | undefined>;
+    };
 }
